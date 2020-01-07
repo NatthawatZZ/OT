0b9aa4dfbc70f04c36c010ebd405a5dd73b48a3e
marknoiz <mataratnatthawat@gmail.com>
2020-01-07 17:08:01 +0700

    Test Push

--- themes/newdemo/views/site/index.php
+++ themes/newdemo/views/site/index.php
@@ -11,7 +11,7 @@
 
 <?php include Yii::app()->basePath .'/../themes/newdemo/include/inc-header.php'; ?>
   <div class="container">
-    <h1 class="intro-title mb-4">OT Bangkok Web Solutions</h1>
+    <h1 class="intro-title mb-4">มาร์ค น้อย</h1>
       <p class="intro-subtitle"><span class="text-slider-items">ระบบค่าตอบแทนการทำงานล่วงเวลา (OT)
         ,ระบบค่าเดินทางพนักงาน และอื่นๆ,
         ระบบรายงาน,บริษัท บางกอก เว็บ โซลูชั่น</span><strong class="text-slider"></strong></p>
